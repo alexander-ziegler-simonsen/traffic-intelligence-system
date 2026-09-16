439987c73fc6b1731494eafbc4e5963a6534aed3
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-09-16 14:59:24 +0200

    added mappers, services and scalar to program - fixed the dbContext - added new packages to the api

--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using api.data;
+using api.Endpoints;
+using api.Interfaces;
+using api.Services;
+using api.Mapping;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +13,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 // dbcontext
-builder.Services.addDbContext<TisDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+builder.Services.AddDbContext<TisDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+
+// mapper
+builder.Services.AddSingleton<StopMapper>();
+builder.Services.AddSingleton<VehicleTypeMapper>();
+builder.Services.AddSingleton<VehicleMapper>();
+builder.Services.AddSingleton<JourneyMapper>();
+builder.Services.AddSingleton<JourneyPathTimePlanMapper>();
+builder.Services.AddSingleton<JourneyPathMapper>();
+builder.Services.AddSingleton<UsingJourneyMapper>();
+builder.Services.AddSingleton<UsingJourneyLogMapper>();
+
+// services
+builder.Services.AddScoped<IVehicleService, VehicleService>();
+builder.Services.AddScoped<IJourneyService, JourneyService>();
 
 var app = builder.Build();
 
@@ -16,10 +35,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
 
-
+app.MapJourneyEndpoints();
 
 app.Run();
